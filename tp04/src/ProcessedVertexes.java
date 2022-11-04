c7e058a1021c4461f1ac898373438f953a57be6e
Iliass Lasri <iliass.lasri@telecom-paris.fr>
2022-11-04 16:15:50 +0100

    initial Commit

---
+++ tp04/src/ProcessedVertexes.java
@@ -0,0 +1,11 @@
+public interface ProcessedVertexes {
+		// cette méthode servera à ajouter le pivot 
+		// dés qu'il soit traité
+		// et le sommet de start
+		
+		public void unionPivot(Vertex);
+		
+		//Cette méthode servera à tester si 
+		// un Vertex est dans les Vertex traités ou non.
+		public boolean isInProcessedVertexes(Vertex);
+}

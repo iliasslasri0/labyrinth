8c3f76b0c05716e9a0e960ef684d27669f4103f2
iliass.lasri <ililass.lasri@telecom-paris.fr>
2022-11-09 17:01:06 +0100

    supprimé :        tp04/bin/.gitignore 	nouveau fichier : tp04/bin/Maze/ArrivalHex.class 	nouveau fichier : tp04/bin/Maze/DepartureHex.class 	nouveau fichier : tp04/bin/Maze/EmptyHex.class 	nouveau fichier : tp04/bin/Maze/Maze.class 	nouveau fichier : tp04/bin/Maze/MazeHex.class 	nouveau fichier : tp04/bin/Maze/WallHex.class 	nouveau fichier : tp04/bin/graph/Dijkstra.class 	nouveau fichier : tp04/bin/graph/Distance.class 	nouveau fichier : tp04/bin/graph/Graph.class 	nouveau fichier : tp04/bin/graph/MinDistance.class 	nouveau fichier : tp04/bin/graph/ProcessedVertexes.class 	nouveau fichier : tp04/bin/graph/ShortestPaths.class 	nouveau fichier : tp04/bin/graph/Vertex.class 	modifié :         tp04/bin/module-info.class 	renommé :         tp04/src/graph/ArrivalHex.java -> tp04/src/Maze/ArrivalHex.java 	renommé :         tp04/src/graph/DepartureHex.java -> tp04/src/Maze/DepartureHex.java 	renommé :         tp04/src/graph/EmptyHex.java -> tp04/src/Maze/EmptyHex.java 	nouveau fichier : tp04/src/Maze/Maze.java 	nouveau fichier : tp04/src/Maze/MazeHex.java 	renommé :         tp04/src/graph/WallHex.java -> tp04/src/Maze/WallHex.java 	modifié :         tp04/src/graph/Dijkstra.java 	modifié :         tp04/src/graph/Graph.java 	supprimé :        tp04/src/graph/MazeHex.java
     Modifications qui ne seront pas validées :
    	modifié :         tp04/bin/Maze/Maze.class
    	modifié :         tp04/src/Maze/Maze.java
    	modifié :         tp04/src/Maze/WallHex.java

---
+++ tp04/src/Maze/MazeHex.java
@@ -0,0 +1,12 @@
+package Maze;
+import java.util.*;
+
+public class MazeHex{
+	int sizeMaze = 10;
+	ArrayList<String> maze=new ArrayList<String>(sizeMaze);
+	
+	public void transformToWallHex(int x,int y) {
+		
+	}
+	
+}
--- tp04/src/graph/Dijkstra.java
+++ tp04/src/graph/Dijkstra.java
@@ -1,8 +1,7 @@
-import java.util.*;
 package graph;
+import java.util.*;
 
 public class Dijkstra {
-	private static final String BigInteger = ;
 
 	public ShortestPaths dijkstra(Graph graph,Vertex startVertex,Vertex endVertex,ProcessedVertexes processedVertexes, MinDistance minDistance,Distance distance) {
 		ProcessedVertexes processedvertexes = new ProcessedVertexes;
@@ -11,14 +10,25 @@ public class Dijkstra {
 		
 		for (Vertex v : graph) {
 			replaceMinDistance(Float.POSITIVE_INFINITY,v);
-		
 		}
 		while (!( isInProcessedVertexes(endVertex))){
 			for (Vertex v:succVertexNotProcce(pivotVertex)) {
-				if (minDistance(pivotVertex) + distance(pivotVertex, v) < minDistance(v)) {
+				if (actuelMinDistance(pivotVertex) + distance(pivotVertex, v) < actuelMinDistance(v)) {
 					replaceMinDistance(minDistance(pivotVertex) + distance(pivotVertex, v), v);
 					previous(v)=pivot;
+					
 				}
+			
+			for (Vertex v : succVertexNotProcce(pivotVertex) ;float i= Float.POSITIVE_INFINITY) {
+				if (actuelMinDistance(v) <=   i) {
+					i = actuelMimnDistance(v);
+					pivotVertex = v;
+					
+					
+				}
+				unionPivot(v);
+				
+			}
 			}
 		}
 	}

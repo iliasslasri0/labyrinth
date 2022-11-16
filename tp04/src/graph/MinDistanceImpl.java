a47879995d2474e07a9e8fe22558b6361fd9e29b
iliass.lasri <ililass.lasri@telecom-paris.fr>
2022-11-16 16:54:27 +0100

    tp9 Dijkstra (TP DSI)

--- tp04/src/graph/MinDistanceImpl.java
+++ tp04/src/graph/MinDistanceImpl.java
@@ -1,5 +1,22 @@
 package graph;
 
-public class MinDistanceImpl {
+public class MinDistanceImpl implements MinDistance {
+
+	@Override
+	public void replaceMinDistance(float d, Vertex V) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int actuelMinDistance(Vertex V) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int distance(Vertex V, Vertex V2) {
+		return 1;
+	}
 
 }
